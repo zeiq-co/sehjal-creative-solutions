4dc25eae65a3d93e4f0c8d6aca944ae45e423251
animusarya <souravarya4u@gmail.com>
2019-08-19 05:55:45 -0700

    minor fixes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,8 @@ class ContactPage extends Component {
       <Layout page={"contact"}>
         <h1 className="title">{contactData.title}</h1>
         <div className="text">
-          We would love to hear from you. Drop us a line.
+          We’re as accessible as your good neighbour. So much so that you can
+          hand us over your keys too.
         </div>
         <div className="contact-grid-container">
           <div className="contact-grid-item">
@@ -32,9 +33,9 @@ class ContactPage extends Component {
               className="contact-icon"
             ></img>
           </div>
-          <div className="contact-grid-item">info@crafty.co</div>
-          <div className="contact-grid-item">1-xxx-xxx-xx67</div>
-          <div className="contact-grid-item">@CraftyCo</div>
+          <div className="contact-grid-item">hi@zeiq.co</div>
+          <div className="contact-grid-item">0181-519-5423 - IN</div>
+          <div className="contact-grid-item">@Zeiqofficial</div>
         </div>
       </Layout>
     )
